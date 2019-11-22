94dc2442d957f2ecdb678d44cc5076ba76396466
Joseph Lucero <jlucero@sfu.ca>
2019-11-22 13:40:24 -0800

    Upload of finally working code

--- project/src/1D/spectral.f90
+++ project/src/1D/spectral.f90
@@ -11,88 +11,343 @@ real(8), parameter :: pi = 4.0*atan(1.0)
 
 contains
 
-subroutine get_steady_spectral_ft_2D( &
-	n, m, dt, check_step, D, dx, dy, &
-	dmu1, dmu2, ddmu1, ddmu2, p_initial, p_final &
-	)
+subroutine get_spectral_steady_ft( &
+    n, m, dt, check_step, D, dx, dy, dmu1, ddmu1, dmu2, ddmu2, p_initial, p_final &
+    )
     integer(8), intent(in) :: n, m
     real(8), intent(in) :: dt
     integer(8), intent(in) :: check_step
-    real(8), intent(in) :: D
-    real(8), intent(in) :: dx, dy
-    real(8), dimension(n,m) :: dmu1, dmu2, ddmu1, ddmu2
-    real(8), dimension(n,m), intent(in) :: p_initial
+    real(8), intent(in) :: D, dx, dy
+    real(8), dimension(n,m), intent(in) :: dmu1, ddmu1, dmu2, ddmu2
+    real(8), dimension(n,m), intent(inout) :: p_initial
     real(8), dimension(n,m), intent(inout) :: p_final
-
-    real(8), dimension(:,:), allocatable :: p_now, p_last, p_last_ref, dv
 
     ! continue condition
     logical cc
     integer(8) step_counter ! counting steps
     real(8) tot_var_dist ! total variation distance
+    
+    complex(8), dimension(n,m) :: pr, phat_mat, p_penultimate
+    complex(8), dimension(n*m+1) :: p_now
+    complex(8), dimension(n*m) :: p_last_ref
+    integer(8) plan0, plan1, status
 
-    allocate(p_now(n,m), p_last(n,m), p_last_ref(n,m), dv(n,m))
+    complex(8), dimension(:), allocatable :: update
 
-    ! initialize based on initial data
-	p_last = p_initial
+    allocate( update(n*m+1) )
+
+    print *, sum(p_initial)
+
+    pr = p_initial
+
+	! call dfftw_init_threads(status)
+	! call dfftw_plan_with_nthreads(2)
+
+	! planning is good
+    call dfftw_plan_dft_2d(plan0,n,m,pr,phat_mat,FFTW_FORWARD,FFTW_ESTIMATE)
+
+    ! take the distribution into k-space
+    call dfftw_execute_dft(plan0,pr,phat_mat)
+
+	call dfftw_destroy_plan(plan0)
+    ! call dfftw_cleanup_threads()
+
+    ! initialize based on fourier transform of initial data
+    p_now(:n*m) = pack(phat_mat,.TRUE.)
+    p_now(n*m+1) = 0.0 ! set time = 0
+
+    phat_mat = 0.0 ! reset the phat_mat matrix
 
     ! initialize reference array
-    p_last_ref = 0.0; p_now = 0.0
+    p_last_ref = 0.0
+
+    ! initialize loop variables
+    cc = .TRUE.; step_counter = 0
+
+    print *, maxval(abs(p_now(:n*m)))
+    print *, maxval(dmu1), maxval(ddmu1), maxval(dmu2), maxval(ddmu2)
+
+    do while (cc)
+
+        ! update probabilities using a Gauss-Legendre scheme
+        call evalf( &
+            n, m, D, dx, dy, &
+            dmu1, dmu2, ddmu1, ddmu2, p_now, update &
+            )
+        print *, "Max value check:", maxval(abs(p_now(:n*m)))
+
+        p_now = p_now + dt*update
+
+        ! bail at the first sign of trouble
+        ! if (abs(sum(p_now(:n*m)) - 1.0) .ge. float32_eps) stop "Normalization broken!"
+
+        if (step_counter .EQ. check_step) then
+            tot_var_dist = 0.5*sum(abs(p_last_ref-p_now(:n*m)))
+
+            print *, "Tot. var. dist = ", tot_var_dist
+
+            if (tot_var_dist < float64_eps) then
+                cc = .FALSE.
+            else
+                tot_var_dist = 0.0; step_counter = 0
+                p_last_ref = p_now(:n*m)
+            end if
+        end if
+        step_counter = step_counter + 1
+    end do
+
+    phat_mat = reshape(p_now(:n*m), [n,m])
+
+    print *, "Phat_mat pass off:", maxval(abs(phat_mat))
+
+	! planning is good
+    call dfftw_plan_dft_2d(plan1,n,m,phat_mat,p_penultimate,FFTW_BACKWARD,FFTW_ESTIMATE)
+
+    p_penultimate = 0.0
+    ! take the distribution back into real-space
+    call dfftw_execute_dft(plan1,phat_mat,p_penultimate)
+    call dfftw_destroy_plan(plan1)
+
+    p_final = 0.0; p_final = realpart(p_penultimate)/(n*m)
+
+    print *, "maxvalue p final:", maxval(p_final)
+    print *, "final normalization:", sum(p_final)
+    
+    if (abs(sum(p_final) - 1.0) .ge. float32_eps) stop "Normalization broken!"
+
+    ! p_final = 0.0; p_final = reshape(p_now(:n*m),[n,m])
+end subroutine get_spectral_steady_ft
+
+subroutine get_spectral_steady_gl10( &
+    n, m, dt, check_step, D, dx, dy, dmu1, ddmu1, dmu2, ddmu2, p_initial, p_final &
+    )
+    integer(8), intent(in) :: n, m
+    real(8), intent(in) :: dt
+    integer(8), intent(in) :: check_step
+    real(8), intent(in) :: D, dx, dy
+    real(8), dimension(n,m), intent(in) :: dmu1, ddmu1, dmu2, ddmu2
+    real(8), dimension(n,m), intent(inout) :: p_initial
+    real(8), dimension(n,m), intent(inout) :: p_final
+
+    ! continue condition
+    logical cc
+    integer(8) step_counter ! counting steps
+    real(8) tot_var_dist ! total variation distance
+    
+    complex(8), dimension(n,m) :: pr, phat_mat, p_penultimate, px_now
+    complex(8), dimension(n*m+1) :: p_now
+    real(8), dimension(n,m) :: p_last_ref
+    integer(8) plan0, plan1, plan2, status
+
+    print *, sum(p_initial)
+
+    pr = p_initial
+
+	call dfftw_init_threads(status)
+	call dfftw_plan_with_nthreads(4)
+
+	! planning is good
+    call dfftw_plan_dft_2d(plan0,n,m,pr,phat_mat,FFTW_FORWARD,FFTW_ESTIMATE)
+    call dfftw_plan_dft_2d(plan1,n,m,phat_mat,px_now,FFTW_BACKWARD,FFTW_ESTIMATE)
+    call dfftw_plan_dft_2d(plan2,n,m,phat_mat,p_penultimate,FFTW_BACKWARD,FFTW_ESTIMATE)
+
+    ! take the distribution into k-space
+    call dfftw_execute_dft(plan0,pr,phat_mat)
+
+	call dfftw_destroy_plan(plan0)
+
+    ! initialize based on fourier transform of initial data
+    p_now(:n*m) = pack(phat_mat,.TRUE.)
+    p_now(n*m+1) = 0.0 ! set time = 0
+
+    phat_mat = 0.0 ! reset the phat_mat matrix
+
+    ! initialize reference array
+    p_last_ref = 0.0
 
     ! initialize loop variables
     cc = .TRUE.; step_counter = 0
 
     do while (cc)
 
-        call update_probability(n, dx, m, dy, dv, p_last, dmu1, ddmu1, dmu2, ddmu2, D)
-
-		print *, sum(p_last)
-		! update using Euler step
-        p_now = p_last + dt*dv
-		print *, sum(p_now)
-
-        ! bail at the first sign of trouble
-        ! if (abs(sum(p_now) - 1.0) .ge. float32_eps) stop "Normalization broken!"
+        ! update probabilities using a Gauss-Legendre scheme
+        call gl10_1D(n, dx, m, dy, dmu1, ddmu1, dmu2, ddmu2, p_now, D, dt)
 
         if (step_counter .EQ. check_step) then
-			tot_var_dist = 0.5*sum(abs(p_last_ref-p_now))
 
-            print *, abs(maxval(dt*dv)), tot_var_dist
+            phat_mat = reshape(p_now(:n*m), [n,m])
+
+            call dfftw_execute(plan1, phat_mat, px_now)
+
+            ! bail at first sign of trouble
+            if (abs(sum(realpart(px_now)/(n*m)) - 1.0) .ge. float32_eps) stop "Normalization broken!"
+
+            ! compute total variation distance
+            tot_var_dist = 0.5*sum(abs(p_last_ref-(realpart(px_now)/(n*m))))
+
+            print *, "Tot. var. dist = ", tot_var_dist
 
             if (tot_var_dist < float64_eps) then
                 cc = .FALSE.
             else
-                tot_var_dist = 0.0; step_counter = 0
-                p_last_ref = p_now
+                tot_var_dist = 0.0; step_counter = 0; phat_mat = 0.0
+                p_last_ref = realpart(px_now)/(n*m)
             end if
         end if
-
-        ! cycle the variables
-        p_last = p_now; p_now = 0.0
         step_counter = step_counter + 1
     end do
 
-    p_final = 0.0; p_final = p_last
+    phat_mat = reshape(p_now(:n*m), [n,m])
 
-end subroutine get_steady_spectral_ft_2D
+    p_penultimate = 0.0
+    ! take the distribution back into real-space
+    call dfftw_execute_dft(plan2,phat_mat,p_penultimate)
+    call dfftw_destroy_plan(plan2)
+    
+    call dfftw_cleanup_threads()
 
-subroutine update_probability(n, dx, m, dy, update, p_last, dmu1, ddmu1, dmu2, ddmu2, D)
-	integer(8), intent(in) :: n, m
-	real(8), intent(in) :: dx, dy
-	real(8), dimension(n,m), intent(inout) :: update
-	real(8), dimension(n,m), intent(in) :: p_last
-	real(8), dimension(n,m), intent(in) :: dmu1, ddmu1, dmu2, ddmu2
-	real(8), intent(in) :: D
-	real(8), dimension(n,m) :: dv, ddv
+    p_final = 0.0; p_final = realpart(p_penultimate)/(n*m)
 
-	! advection part
-	call divergence(n, dx, m, dy, dmu1, ddmu1, dmu2, ddmu2, p_last, dv)
+    print *, "maxvalue p final:", maxval(p_final)
+    print *, "final normalization:", sum(p_final)
+    
+    if (abs(sum(p_final) - 1.0) .ge. float32_eps) stop "Normalization broken!"
 
-	! diffusion part
-	call laplacian(n, dx, m, dy, p_last, ddv)
+    ! p_final = 0.0; p_final = reshape(p_now(:n*m),[n,m])
+end subroutine get_spectral_steady_gl10
 
-	update = D*(-dv + ddv)
-end subroutine update_probability
+! 10th order implicit Gauss-Legendre integrator
+subroutine gl10_1D(n, dx, m, dy, dmu1, ddmu1, dmu2, ddmu2, p, D, dt)
+    integer(8), parameter :: s = 5 ! number of stages
+    integer(8), intent(in) :: n, m
+    real(8), intent(in) :: dx, dy
+    real(8), dimension(n,m), intent(in) :: dmu1, ddmu1, dmu2, ddmu2
+    complex(8), dimension(n*m+1), intent(inout) :: p
+    real(8), intent(in) :: D, dt
+    complex(8), dimension(:), allocatable :: update
+    complex(8), dimension(:,:), allocatable :: g
+    integer(8) i, k
+
+    ! Butcher tableau for 10th order Gauss-Legendre method
+    complex(8), parameter :: a(s,s) = reshape((/ &
+                0.5923172126404727187856601017997934066Q-1, -1.9570364359076037492643214050884060018Q-2, &
+                1.1254400818642955552716244215090748773Q-2, -0.5593793660812184876817721964475928216Q-2, &
+                1.5881129678659985393652424705934162371Q-3,  1.2815100567004528349616684832951382219Q-1, &
+                1.1965716762484161701032287870890954823Q-1, -2.4592114619642200389318251686004016630Q-2, &
+                1.0318280670683357408953945056355839486Q-2, -2.7689943987696030442826307588795957613Q-3, &
+                1.1377628800422460252874127381536557686Q-1,  2.6000465168064151859240589518757397939Q-1, &
+                1.4222222222222222222222222222222222222Q-1, -2.0690316430958284571760137769754882933Q-2, &
+                4.6871545238699412283907465445931044619Q-3,  1.2123243692686414680141465111883827708Q-1, &
+                2.2899605457899987661169181236146325697Q-1,  3.0903655906408664483376269613044846112Q-1, &
+                1.1965716762484161701032287870890954823Q-1, -0.9687563141950739739034827969555140871Q-2, &
+                1.1687532956022854521776677788936526508Q-1,  2.4490812891049541889746347938229502468Q-1, &
+                2.7319004362580148889172820022935369566Q-1,  2.5888469960875927151328897146870315648Q-1, &
+                0.5923172126404727187856601017997934066Q-1/), (/s,s/))
+    complex(8), parameter ::   b(s) = [ &
+                1.1846344252809454375713202035995868132Q-1,  2.3931433524968323402064575741781909646Q-1, &
+                2.8444444444444444444444444444444444444Q-1,  2.3931433524968323402064575741781909646Q-1, &
+                1.1846344252809454375713202035995868132Q-1]
+
+    allocate( g(n*m+1,s), update(n*m+1) )
+
+    ! iterate trial steps
+    g = 0.0; do k = 1,16
+            g = matmul(g,a)
+            do i = 1,s
+                call evalf(n, m, D, dx, dy, dmu1, dmu2, ddmu1, ddmu2, p+g(:,i)*dt, g(:,i))
+            end do
+    end do
+
+    update = matmul(g,b)*dt
+
+    ! update the solution
+    p = p + update
+end subroutine gl10_1D
+
+subroutine evalf( &
+	n, m, D, dx, dy, &
+	dmu1, dmu2, ddmu1, ddmu2, in_array, update &
+	)
+    integer(8), intent(in) :: n, m
+    real(8), intent(in) :: D
+    real(8), intent(in) :: dx, dy
+    real(8), dimension(n,m), intent(in) :: dmu1, dmu2, ddmu1, ddmu2
+    complex(8), dimension(n*m+1), intent(in) :: in_array
+    complex(8), dimension(n*m+1), intent(inout) :: update
+
+	! declare interior variables
+    complex(8), dimension(n,m) :: pr, phat0, phat1, phat2, phat3, phat4
+    complex(8), dimension(n,m) :: out0, out1, out2, out3, out4
+	integer(8) plan0, plan1, plan2, plan3, plan4, plan5, plan6, plan7
+	integer(8) status
+    integer(8) i, j
+
+	real(8) t
+
+    real(8), dimension(:,:), allocatable :: kx, ky, kkx, kky
+    
+    allocate( kx(n,m), ky(n,m) )
+
+    ! process the input array
+    pr = 0.0; pr = reshape(in_array(:n*m), [n,m])
+    t = in_array(n*m+1)
+
+	! get the frequencies ready
+	do i=1,n
+        kx(:,i) = fftfreq(n, dx)
+		ky(i,:) = fftfreq(m, dy)
+	end do
+
+    ! initialize real-space arrays
+    out0 = 0.0; out1 = 0.0; out2 = 0.0; out3 = 0.0; out4 = 0.0
+    ! initialize k-space arrays
+    phat0 = 0.0; phat1 = 0.0; phat2 = 0.0; phat3 = 0.0; phat4 = 0.0
+
+    ! plan all of the fft's that are to be done
+
+    ! inverse fourier transforms back to real-space
+	call dfftw_plan_dft_2d(plan0,n,m,pr,out0,FFTW_BACKWARD,FFTW_ESTIMATE)
+    call dfftw_plan_dft_2d(plan1,n,m,phat1,out1,FFTW_BACKWARD,FFTW_ESTIMATE)
+	call dfftw_plan_dft_2d(plan2,n,m,phat2,out2,FFTW_BACKWARD,FFTW_ESTIMATE)
+
+    ! forward fourier transforms to k-space
+	call dfftw_plan_dft_2d(plan3,n,m,out0,phat0,FFTW_FORWARD,FFTW_ESTIMATE)
+	call dfftw_plan_dft_2d(plan4,n,m,out1,phat1,FFTW_FORWARD,FFTW_ESTIMATE)
+    call dfftw_plan_dft_2d(plan5,n,m,out2,phat2,FFTW_FORWARD,FFTW_ESTIMATE)
+
+	phat1 = (0.0,1.0)*kx*pr; phat1(n/2+1,:) = 0.0
+    phat2 = (0.0,1.0)*ky*pr; phat2(:,m/2+1) = 0.0
+    phat3 = -D*( kx**2+ky**2 )*pr
+
+    ! go back into real space
+	call dfftw_execute_dft(plan0, pr, out0)
+	call dfftw_execute_dft(plan1, phat1, out1)
+	call dfftw_execute_dft(plan2, phat2, out2)
+
+    ! do multiplications in real space
+	out0 = -D*(ddmu1+ddmu2)*(realpart(out0)/(n*m))
+	out1 = -D*dmu1*(realpart(out1)/(n*m))
+    out2 = -D*dmu2*(realpart(out2)/(n*m))
+
+    ! reset the k-space arrays
+	phat0 = 0.0; phat1 = 0.0; phat2 = 0.0
+
+    ! go back into fourier space
+	call dfftw_execute_dft(plan3, out0, phat0)
+	call dfftw_execute_dft(plan4, out1, phat1)
+	call dfftw_execute_dft(plan5, out2, phat2)
+    
+	call dfftw_destroy_plan(plan0)
+	call dfftw_destroy_plan(plan1)
+    call dfftw_destroy_plan(plan2)
+	call dfftw_destroy_plan(plan3)
+	call dfftw_destroy_plan(plan4)
+    call dfftw_destroy_plan(plan5)
+
+    update = 0.0
+    update(:n*m) = pack(phat0 + phat1 + phat2 + phat3,.TRUE.)
+    update(n*m+1) = 1.0
+end subroutine evalf
 
 subroutine divergence(n, dx, m, dy, dmu1, ddmu1, dmu2, ddmu2, in_array, out_array)
 	integer(8), intent(in) :: n, m
