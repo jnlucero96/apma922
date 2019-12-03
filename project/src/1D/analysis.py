aeb43205efe4fece9cb0eede921cc0ea3f98556f
Joseph Lucero <jlucero@sfu.ca>
2019-12-02 16:39:32 -0800

    Adding analysis for the error and for creation of movies

--- project/src/1D/analysis.py
+++ project/src/1D/analysis.py
@@ -1,64 +1,250 @@
 #!/usr/bin/env python3
 
-from numpy import loadtxt
+from math import pi
+import numpy as np
 
 from matplotlib.style import use
-
 use("dark_background")
-
+from matplotlib.cm import get_cmap
 from pltconfig import *
+from matplotlib.pyplot import figure
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+from matplotlib.animation import FuncAnimation
+
+from initialize import problem_2D
 
 def get_params():
 
     # discretization parameters
     dt = 5e-1  # time discretization. Keep this number low
-    N = 360 # inverse space discretization. Keep this number high!
+    N = 60  # inverse space discretization. Keep this number high!
+    M = 60  # inverse space discretization. Keep this number high!
 
-    # model-specific parameters
-    gamma = 1000.0  # drag
-    beta = 1.0  # 1/kT
-    m = 1.0  # mass
+    psi1 = 2.0  # force on system by chemical bath B1
+    psi2 = -1.0  # force on system by chemical bath B2
 
-    E = 2.0 # energy scale of system
+    steady = False # evolve to steady-state distribution
 
-    psi1 = 0.0 # force on system by chemical bath B1
-    psi2 = 0.0 # force on system by chemical bath B2
+    # modes for solver:
+    # 1 := forward-euler
+    # 2 := imex
+    # 3 := rk2
+    # 4 := rk4
+    # 5 := rk4 - integrating factor
+    # 6 := rk4 - exponential time differencing
+    # 7 := gl04
+    # 8 := gl06
+    # 9 := gl08
+    # 10 := gl10
 
-    n = 3.0 # number of minima in system potential
+    scheme = 2
 
-    mode = "spec_addif"
+    return ( dt, N, M, psi1, psi2, steady, scheme )
 
-    dim = 1
+def plot_equilibrium():
 
-    return ( dt, N, gamma, beta, m, E, psi1, psi2, n, mode, dim )
-
-def analyze():
-
-    [ dt, N, gamma, beta, m, E, psi1, psi2, n, mode, dim ] = get_params()
+    [ dt, N, M, psi0, psi1, steady, scheme ] = get_params()
 
     target_dir = './master_output_dir/'
-    data_filename = f'/ref_{dim}D_E_{E}_psi1_{psi1}_psi2_{psi2}_n_{n}_outfile.dat'
-    data_total_path = target_dir + data_filename
 
-    positions, p_ss, p_equil = loadtxt(
-        data_total_path, usecols=(0,1,3), unpack=True
-        )
+    problem = problem_2D(
+        x0=0.0, xn=2.0*pi, y0=0.0, ym=2.0*pi, n=N, m=M,
+        E0=2.0, Ec=8.0, E1=2.0, num_minima0=3.0, num_minima1=3.0,
+        D=0.001, psi0=psi0, psi1=psi1
+    )
+
+    xx, yy = np.meshgrid(problem.theta0, problem.theta1, indexing="ij")
 
     fig, ax = subplots(1,1)
-    ax.set_ylabel(r"$P(\theta)$", fontsize=28)
-    ax.set_xlabel(r"$\theta$", fontsize=28)
+    ax.set_xlabel(r"$\theta_{0}$", fontsize=28)
+    ax.set_ylabel(r"$\theta_{1}$", fontsize=28)
 
-    ax.plot(positions, p_equil, "r--", label=r"$\pi^{\mathrm{eq}}$")
-    ax.plot(positions, p_ss, "o", ms=7.0, label=r"$p^{\mathrm{ss}}$")
+    temp_fig, temp_ax = subplots(1,1)
+    cs = temp_ax.contourf(
+        xx, yy,
+        np.linspace(
+            0.0, 1.05*problem.p_equil.max(),
+            problem.n*problem.m
+            ).reshape(problem.n, problem.m), 30,
+        vmin=0.0, vmax=problem.p_equil.max(), cmap=get_cmap("Purples_r")
+        )
 
-    ax.legend(loc=0, prop={"size": 18})
-    ax.ticklabel_format(style="sci",scilimits=(0,0))
+    ax.contourf(
+        problem.theta0, problem.theta1, problem.p_equil, 30,
+        vmin=0.0, vmax=problem.p_equil.max(), cmap=get_cmap("Purples_r")
+        )
 
     fig.tight_layout()
-    fig.savefig("figure.pdf")
+
+    left = 0.1
+    right = 0.85
+    bottom = 0.1
+    top = 0.925
+
+    fig.subplots_adjust(left=left, right=right, bottom=bottom, top=top)
+
+    cax = fig.add_axes([0.88,0.10, 0.02, 0.825])
+    cbar = fig.colorbar(cs, cax=cax, orientation="vertical", ax=ax)
+    cbar.formatter.set_scientific(True)
+    cbar.formatter.set_powerlimits((0,0))
+    cbar.ax.tick_params(labelsize=24)
+    cbar.ax.yaxis.offsetText.set_fontsize(24)
+    cbar.ax.yaxis.offsetText.set_x(2.75)
+    cbar.update_ticks()
+
+    fig.savefig(target_dir + f"/equilibrium_N_{N}_M_{M}_psi0_{psi0}_psi1_{psi1}_figure.pdf")
+
+def analyze_oversight():
+
+    [ _, _, _, psi0, psi1, steady, scheme ] = get_params()
+
+    target_dir = './master_output_dir/'
+    data_dir = "/Users/jlucero/data_dir/2019-12-02/"
+
+    dtlist = [5e-1, 1e-1, 1e-2, 1e-3]
+
+    spatial_type = "fd"
+
+    Ndicts = {}; tdicts = {}; errdicts = {}
+
+    for dt in dtlist:
+
+        data_filename = f'/{spatial_type}_oversight_scheme_{scheme}_dt_{dt}_file.dat'
+
+        try:
+            data_array  = np.loadtxt(data_dir + data_filename)
+
+            if not (len(data_array.shape) == 1):
+                data_array = data_array[data_array[:,0].argsort()]
+                Ndicts[dt] = data_array[:,0]
+                tdicts[dt] = data_array[:,3]
+                errdicts[dt] = data_array[:,4]
+            else:
+                Ndicts[dt] = np.array([data_array[0]])
+                tdicts[dt] = np.array([data_array[3]])
+                errdicts[dt] = np.array([data_array[4]])
+
+        except OSError:
+            print(f"{dt} is missing. Moving to next")
+
+    fig, ax = subplots(1,1)
+    ax.set_ylabel(r"$t_{\mathrm{convergence}}$", fontsize=38)
+    ax.set_xlabel(r"$N$", fontsize=38)
+
+    for ii, dt in enumerate(dtlist):
+
+        try:
+            ax.plot(Ndicts[dt], tdicts[dt], "o-", color=f"C{ii}", ms=12, label=rf"${dt}$")
+        except KeyError:
+            print(f"{dt} not present. Not plotting as result.")
+
+    ax.set_xlim([60.0, 361.0])
+    ax.set_ylim([1e-3, 1e3])
+    ax.set_yscale("log")
+    ax.grid(True)
+    ax.legend(loc=0, prop={"size": 22}, title=r"$k$", title_fontsize=28)
+    ax.tick_params(labelsize=34)
+
+    fig.tight_layout()
+    fig.savefig(target_dir + f"/{spatial_type}_scheme_{scheme}_time_figure.pdf")
+
+    fig2, ax2 = subplots(1,1)
+    ax2.set_ylabel(r"$E(N)$", fontsize=38)
+    ax2.set_xlabel(r"$N$", fontsize=38)
+
+    for ii, dt in enumerate(dtlist):
+
+        try:
+            ax2.plot(Ndicts[dt], errdicts[dt], "o-", color=f"C{ii}", ms=12, label=rf"${dt}$")
+        except KeyError:
+            print(f"{dt} not present. Not plotting as result.")
+
+    ax2.set_xlim([60.0, 361.0])
+    ax2.set_ylim([1e-18, 1e-2])
+    ax2.set_yscale("log")
+    ax2.grid(True)
+    ax2.legend(loc=0, prop={"size": 22}, title=r"$k$", title_fontsize=24)
+    ax2.tick_params(labelsize=34)
+
+    fig2.tight_layout()
+    fig2.savefig(target_dir + f"/{spatial_type}_scheme_{scheme}_err_figure.pdf")
+
+def trace():
+
+    [ dt, N, M, psi0, psi1, steady, scheme ] = get_params()
+
+    target_dir = './master_output_dir/'
+    data_filename = f'/evol_scheme_{scheme}_dt_{dt}_N_{N}_M_{M}_psi0_{psi0}_psi1_{psi1}_outfile.dat'
+    data_total_path = target_dir + data_filename
+
+    problem = problem_2D(
+        x0=0.0, xn=2.0*pi, y0=0.0, ym=2.0*pi, n=N, m=M,
+        E0=2.0, Ec=8.0, E1=2.0, num_minima0=3.0, num_minima1=3.0,
+        D=0.001, psi0=psi0, psi1=psi1
+    )
+
+    xx, yy = np.meshgrid(problem.theta0, problem.theta1, indexing="ij")
+
+    all_data = np.loadtxt(data_total_path)
+
+    data_max = all_data.__abs__().max()
+
+    reverse_purples = get_cmap("Purples_r")
+
+    fig = figure()
+
+    temp_fig, temp_ax = subplots(1,1)
+    cs = temp_ax.contourf(
+        xx, yy,
+        np.linspace(
+            0.0, 1.05*all_data.__abs__().max(),
+            problem.n*problem.m
+            ).reshape(problem.n, problem.m), 30,
+        vmin=0.0, vmax=data_max, cmap=reverse_purples
+        )
+
+    ax = fig.add_subplot(1,1,1)
+    ax.set_ylabel(r"$\theta_{1}$", fontsize=28)
+    ax.set_xlabel(r"$\theta_{0}$", fontsize=28)
+    ax.set_xlim([problem.x0, problem.xn])
+    ax.set_ylim([problem.y0, problem.ym])
+    
+
+    fig.tight_layout()
+
+    left = 0.1
+    right = 0.85
+    bottom = 0.1
+    top = 0.925
+
+    fig.subplots_adjust(left=left, right=right, bottom=bottom, top=top)
+
+    cax = fig.add_axes([0.88,0.10, 0.02, 0.825])
+    cbar = fig.colorbar(cs, cax=cax, orientation="vertical", ax=ax)
+    cbar.formatter.set_scientific(True)
+    cbar.formatter.set_powerlimits((0,0))
+    cbar.ax.tick_params(labelsize=24)
+    cbar.ax.yaxis.offsetText.set_fontsize(24)
+    cbar.ax.yaxis.offsetText.set_x(2.75)
+    cbar.update_ticks()
+
+    cset = ax.contourf(
+            xx, yy, all_data[:,0].reshape(problem.n, problem.m).T,
+            30, vmin=0.0, vmax=data_max, cmap=reverse_purples
+            )
+
+    def update(frame_number, data_array):
+        cset = ax.contourf(
+            xx, yy, data_array[:,frame_number].reshape(problem.n, problem.m).T,
+            30, vmin = 0.0, vmax=data_max, cmap=reverse_purples
+            )
+        return cset
+
+    ani = FuncAnimation(fig, update, all_data.shape[1], fargs=(all_data,))
+
+    ani.save(target_dir + f"evol_scheme_{scheme}_dt_{dt}_N_{N}_M_{M}_psi0_{psi0}_psi1_{psi1}_ani.mp4")
 
 if __name__ == "__main__":
-    analyze()
-
-
-
+    # analyze_oversight()
+    trace()
+    # plot_equilibrium()
